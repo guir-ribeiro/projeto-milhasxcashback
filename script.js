ce4b824eed71963fa941aefb33702083f2b7f1c8
Guilherme Ribeiro Lucio <95583896+guir-ribeiro@users.noreply.github.com>
2022-04-06 14:55:20 -0300

    otimização de código JavaScript
    Notei que meu código havia expressões repetidas  sem necessidades.

--- script.js
+++ script.js
@@ -7,18 +7,18 @@ function calcular(){
     var pn = Number(p.value);
     var res = document.querySelector('div#resultado');
     var cashback = vn * (cn/100);
-    var pontos = (vn * pn / 500) * 20;
+    var pontosminimo = (vn * pn / 500) * 19;
+    var pontosMaximo = (vn * pn / 500) * 21;
 
-    if(cashback > pontos){
+
+    if(cashback > pontosminimo){
         res.innerHTML = 'Compensa mais usando cashback!';
-        res.innerHTML += `<p> O valor em cashback seria ${cashback},00.  </p>`
-        res.innerHTML += `<p> O valor em milhas seria ${pontos},00. `
-        res.innerHTML += `(Levando em conta que seria realizado a transferência com 100% de bonificação e calculando o milheiro a R$ 20,00 reais)</p>`
     }
 
     else{
         res.innerHTML = 'Compensa mais através dos pontos!';
-        res.innerHTML += `<p> O valor em cashback seria R$ ${cashback},00.  </p>`;
-        res.innerHTML += `(Levando em conta que seria realizado a transferência com 100% de bonificação e calculando o milheiro a R$ 20,00 reais)</p>`;
     }
+    res.innerHTML += `<p> O valor em cashback seria R$ ${cashback},00.  </p>`;
+    res.innerHTML += `<p> O valor em milhas seria entre R$ ${pontosminimo},00 e R$ ${pontosMaximo},00. `
+    res.innerHTML += `(Levando em conta que seria realizado a transferência com 100% de bonificação e calculando o valor do milheiro entre R$ 19,00 e R$ 20,00 reais.)</p>`;
 } 
